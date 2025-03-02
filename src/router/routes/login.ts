d9596deefc88d4eedaf5055eeb2fd47eb7e9e6bf
Quique Supertramp <quiquesupertramp@gmail.com>
2025-03-02 04:05:13 +0100

    fix

--- src/router/routes/login.ts
+++ src/router/routes/login.ts
@@ -1,10 +1,11 @@
 import { LAYOUTS } from '@/lib/layouts'
 import type { RouteRecordRaw } from 'vue-router'
+import LoginView from '@/views/login/LoginView.vue'
 
 export const loginRoute: RouteRecordRaw = {
   path: '/login',
   name: 'login',
-  component: () => import('@/views/login/LoginView.vue'),
+  component: LoginView,
   meta: {
     layout: LAYOUTS.EMPTY,
   },
